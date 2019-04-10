10855b6f78abf09d8dc5e2778f0b6268a073b6a7
Ana Teresa Neto <ateresa.neto@gmail.com>
2019-04-10 20:05:11 +0100

    Replace create_tables with SQLAlchemy method.

--- code/resources/item.py
+++ code/resources/item.py
@@ -1,7 +1,6 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
-import sqlite3
 
 
 class Item(Resource):
@@ -60,17 +59,6 @@ class Item(Resource):
 
 
 class ItemList(Resource):
-    TABLE_NAME = 'items'
 
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM {table}".format(table=self.TABLE_NAME)
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-        connection.close()
-
-        return {'items': items}
+        return {'items': [item.json() for item in ItemModel.query.all()]}
