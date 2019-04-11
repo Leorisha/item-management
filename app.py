61364e444fcc07a7250b8d9d3e48746ac0c5d595
Ana Teresa Neto <ateresa.neto@gmail.com>
2019-04-11 19:34:13 +0100

    Fix db problem.

--- app.py
+++ app.py
@@ -15,12 +15,6 @@ app.secret_key = 'jose'
 
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
@@ -30,6 +24,4 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
     app.run(port='8000', debug=True)  # important to mention debug=True
