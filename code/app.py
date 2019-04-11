a7617323af06d6c1ff7951d93a23c9f13fb94ca5
Ana Teresa Neto <ateresa.neto@gmail.com>
2019-04-11 13:32:13 +0100

    Add store resource and endpoints.

--- code/app.py
+++ code/app.py
@@ -5,6 +5,7 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
@@ -25,6 +26,8 @@ jwt = JWT(app, authenticate, identity)
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
     from db import db
